e9bf6e0861a6d7493661ba993b2a60f39efdb707
NullCube <nullcub3@gmail.com>
2023-11-19 19:50:02 -0800

    Actual refactor this time

--- .scripts/video-mover.sh
+++ .scripts/video-mover.sh
@@ -10,18 +10,30 @@ finalFolder=""
 # Make sure to add the full file extension
 finalName=""
 
+################################################
+###                                          ###
+###  Below is the code, above is the config  ###
+###                                          ###
+################################################
+
 if [ -z "${downloadFolder}" ] || [ -z "$finalFolder" ] || [ -z "$finalName" ]; then
 	echo "Error, script not set up properly! Please configure the script before running."
 	exit
 elif ! [ -d "${downloadFolder}" ]; then
 	echo "Error, download folder does not exist"
 	exit
+elif ! [ -r "${downloadFolder}" ]; then
+	echo "Error, download folder cannot be read. Please adjust folder permissions"
+	exit
 elif ! [ -d "${finalFolder}" ]; then
 	echo "Error, final folder does not exist"
 	exit
+elif ! [ -w "${finalFolder}" ]; then
+	echo "Error, final folder cannot be written to. Please adjust folder permissions"
+	exit
 fi
 
-cd "${downloadFolder}" || echo "Error changing directory" && exit
+cd "${downloadFolder}" || exit
 
 recentFile=$(find . -maxdepth 1 -type f -printf '%T@ %p\n' | sort -n | tail -1 | cut -f2- -d" ")
 
