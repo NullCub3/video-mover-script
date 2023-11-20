a622957b81da057f4bb082a0d913f6aeff68a697
NullCube <nullcub3@gmail.com>
2023-11-19 17:08:25 -0800

    First script draft

---
+++ video-mover.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Absolute path of folder the video is first downloaded in
+downloadFolder=""
+
+# Absolute path of folder to move the video into
+finalFolder=""
+
+# Name of desired final video file
+# Make sure to add the full file extension
+finalName=""
+
+if [ -d ${downloadFolder} ]; then
+	cd ${downloadFolder}
+	recentFile=$(ls -Art | tail -n 1)
+	if [ -d ${finalFolder} ]; then
+		mv ./${recentFile} $finalFolder/$finalName
+		echo "File move complete"
+		exit
+	fi
+	echo "Error, final folder does not exist"
+fi
+echo "Error, download folder does not exist"
